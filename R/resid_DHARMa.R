1193e2f958674c624b3e69b7112a79dc6137b915
mariosandoval-mx <sandoval.m@hotmail.com>
2021-10-06 23:45:29 -0400

    resid.DHARMa update

--- R/resid_DHARMa.R
+++ R/resid_DHARMa.R
@@ -1,7 +1,7 @@
 #' Grafico de residuales de un modelo LM, LMM, GLM, GLMM, GAM, etc
 #'
 #' Muestra el ajuste del modelo LM, GLM, GLMM, GAM, etc. Esta función crea residuos escalados
-#' mediante la simulación del modelo ajustado. Los residuos pueden ser extraídos con residuals.DHARMa. Ver
+#' mediante la simulación del modelo ajustado, 5000 simulaciones por default. Los residuos pueden ser extraídos con residuals.DHARMa. Ver
 #' testResiduals para una visión general de las pruebas de residuos, plot.DHARMa para una visión general de
 #' los gráficos disponibles. Esta función fue creada para explorar el ajuste, normalidad y homogeneidad de
 #' varianzas entre los grupos.
@@ -19,7 +19,7 @@
 #' @importFrom DHARMa simulateResiduals
 resid_DHARMa <- function(Modelo){
 
-  res.mod <-DHARMa::simulateResiduals(Modelo,n = 20000)
+  res.mod <-DHARMa::simulateResiduals(Modelo,n = 5000)
 
   base::message(c("Kolmogorov-Smirnov test (KS test) p < 0.05, entonces no hay normalidad en los residuales. Outlier test: p < 0.05, no hay outliers. Grafico de la derecha muestra la distribucin esperada de los resiudales, mediante simulaciones. Esto es util cuando no sabemos cual es la distribucion nula de los residuales. El grafico de la derecha muestra los residuales contra los valores esperados (ajustados). Estas lineas pueden no ajustarse debido a un tamano de muestra reducido."))
 
