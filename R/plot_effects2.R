41ba1797894b7a4d209e3044b92080dbefa8d447
mariosandoval-mx <sandoval.m@hotmail.com>
2022-01-06 20:21:04 -0600

    Plot effects, grids update

--- R/plot_effects2.R
+++ R/plot_effects2.R
@@ -5,6 +5,7 @@
 #' @param modelo Modelo LM, GLM, GLMM, etc.
 #' @param lineas Argumento logico TRUE o FALSE, que indica si se agregan las lineas o no.
 #' @param puntos Argumento logico TRUE o FALSE, que indica si se agregan los puntos o no.
+#' @param grids  Si se especifica F o FALSE, se eliminan las rejillas del grafico.
 #'
 #' @return Grafica todas las variables explicativas del modelo.
 #' @export
@@ -13,17 +14,24 @@
 #' data(iris)
 #' modelo <- glm(Petal.Width ~ Petal.Length*Species+ Sepal.Length, family = gaussian("log"), data=iris)
 #' plot_effects2(modelo, lineas = TRUE, puntos=TRUE)
-#' #plot_effects2(modelo, lineas = FALSE, puntos=FALSE)
-#' #plot_effects2(modelo, lineas = TRUE, puntos=FALSE)
-#' #plot_effects2(modelo, lineas = FALSE, puntos=TRUE)
-
+#' #plot_effects2(modelo)
+#' #plot_effects2(modelo, grids = F)
+#' #plot_effects2(modelo, lineas = T, puntos = T)
+#' #plot_effects2(modelo, lineas = T, puntos = T, grids = F)
+#' #plot_effects2(modelo, lineas=F, puntos = F)
+#' #plot_effects2(modelo, lineas=F, puntos = F, grids = F)
+#' #plot_effects2(modelo, lineas = T, puntos = F)
+#' #plot_effects2(modelo, lineas = T, puntos = F, grids = F)
+#' #plot_effects2(modelo, lineas = F, puntos = T)
+#' #plot_effects2(modelo, lineas = F, puntos = T, grids = F)
 #' @encoding UTF-8
 #' @importFrom ggeffects ggeffect
 #' @importFrom ggpubr ggarrange
 #' @importFrom graphics plot
 #' @importFrom ggpubr grids
-plot_effects2<- function(modelo, lineas=NULL, puntos=NULL) {
-  if(is.null(lineas) & is.null(puntos)){
+plot_effects2<- function(modelo, lineas=NULL, puntos=NULL, grids=NULL) {
+  #### grafico default sin puntos ni lineas con grids
+  if(is.null(lineas) && is.null(puntos) && is.null(grids)){
     th<- ggplot2::theme(axis.text=ggplot2::element_text(size=15,color="black"),axis.title=ggplot2::element_text(size=15,face="bold",color="black"))+ ggplot2::theme_light(base_size = 17)+ ggpubr::grids(linetype = "dashed", color = "grey85")
     #graficar efectos paqueteria ggeffects
     dat<- ggeffects::ggeffect(modelo)
@@ -36,8 +44,22 @@ plot_effects2<- function(modelo, lineas=NULL, puntos=NULL) {
 
     })
     do.call(ggpubr::ggarrange,graphs)
+    ### grafico sin grids ni puntos y lineas
+  }  else if(is.null(lineas) && is.null(puntos) && grids==F){
+    th<- ggplot2::theme(axis.text=ggplot2::element_text(size=15,color="black"),axis.title=ggplot2::element_text(size=15,face="bold",color="black"))+ ggplot2::theme_light(base_size = 17)+ ggpubr::grids(linetype = "dashed", color = "white")
+    #graficar efectos paqueteria ggeffects
+    dat<- ggeffects::ggeffect(modelo)
 
-  } else if( lineas == T & puntos ==  T ){
+    # graficar los efectos y agruparlos en una sola grafica ggpubr
+
+    graphs <- lapply(names(dat),function(x){
+      graphics::plot(dat[[x]], connect.lines = FALSE, add.data=FALSE, colors = "bw", alpha = 0.15, dot.alpha = 0.15,jitter = 0.2,line.size = 1, show.title = FALSE)+th
+
+
+    })
+    do.call(ggpubr::ggarrange,graphs)
+    # grafico con lineas y puntos con grids
+  } else if( lineas == T && puntos ==  T && is.null(grids)){
     th<- ggplot2::theme(axis.text=ggplot2::element_text(size=15,color="black"),axis.title=ggplot2::element_text(size=15,face="bold",color="black"))+ ggplot2::theme_light(base_size = 17)+ ggpubr::grids(linetype = "dashed", color = "grey85")
 
     #graficar efectos paqueteria ggeffects
@@ -51,7 +73,22 @@ plot_effects2<- function(modelo, lineas=NULL, puntos=NULL) {
 
     })
     do.call(ggpubr::ggarrange,graphs)
-  } else if( lineas ==  F & puntos == F) {
+    #grafico con lineas y puntos sin grids
+  } else if(lineas == T && puntos ==  T   && grids==F){
+    th<- ggplot2::theme(axis.text=ggplot2::element_text(size=15,color="black"),axis.title=ggplot2::element_text(size=15,face="bold",color="black"))+ ggplot2::theme_light(base_size = 17) + ggpubr::grids(linetype = "dashed", color = "white")
+    #graficar efectos paqueteria ggeffects
+    dat<- ggeffects::ggeffect(modelo)
+
+    # graficar los efectos y agruparlos en una sola grafica ggpubr
+
+    graphs <- lapply(names(dat),function(x){
+      graphics::plot(dat[[x]], connect.lines = TRUE, add.data=TRUE, colors = "bw", alpha = 0.15, dot.alpha = 0.15,jitter = 0.2,line.size = 1, show.title = FALSE)+th
+
+
+    })
+    do.call(ggpubr::ggarrange,graphs)
+    #grafico sin lineas ni puntos con grid
+  } else if( lineas ==  F && puntos == F && is.null(grids)) {
     th<- ggplot2::theme(axis.text=ggplot2::element_text(size=15,color="black"),axis.title=ggplot2::element_text(size=15,face="bold",color="black"))+ ggplot2::theme_light(base_size = 17)+ ggpubr::grids(linetype = "dashed", color = "grey85")
     #graficar efectos paqueteria ggeffects
     dat2<- ggeffects::ggeffect(modelo)
@@ -64,8 +101,22 @@ plot_effects2<- function(modelo, lineas=NULL, puntos=NULL) {
 
     })
     do.call(ggpubr::ggarrange,graphs2)
+    #grafico solo con lineas sin puntos y con grid
+  } else if( lineas ==  F && puntos == F && grids==F) {
+    th<- ggplot2::theme(axis.text=ggplot2::element_text(size=15,color="black"),axis.title=ggplot2::element_text(size=15,face="bold",color="black"))+ ggplot2::theme_light(base_size = 17)+ ggpubr::grids(linetype = "dashed", color = "white")
+    #graficar efectos paqueteria ggeffects
+    dat2<- ggeffects::ggeffect(modelo)
 
-  } else if( lineas == T & puntos == F) {
+    # graficar los efectos y agruparlos en una sola grafica ggpubr
+
+    graphs2 <- lapply(names(dat2),function(x){
+      graphics::plot(dat2[[x]],connect.lines = FALSE, add.data=FALSE, colors = "bw", alpha = 0.15, dot.alpha = 0.15,jitter = 0.2,line.size = 1, show.title = FALSE)+th
+
+
+    })
+    do.call(ggpubr::ggarrange,graphs2)
+    #grafico solo con lineas sin puntos y con grid
+  }  else if( lineas == T && puntos == F && is.null(grids)) {
     th<- ggplot2::theme(axis.text=ggplot2::element_text(size=15,color="black"),axis.title=ggplot2::element_text(size=15,face="bold",color="black"))+ ggplot2::theme_light(base_size = 17)+ ggpubr::grids(linetype = "dashed", color = "grey85")
     #graficar efectos paqueteria ggeffects
     dat2<- ggeffects::ggeffect(modelo)
@@ -78,9 +129,37 @@ plot_effects2<- function(modelo, lineas=NULL, puntos=NULL) {
 
     })
     do.call(ggpubr::ggarrange,graphs2)
+    #grafico con lineas sin puntos y sin grids
+  }  else if( lineas == T && puntos == F && grids==F) {
+    th<- ggplot2::theme(axis.text=ggplot2::element_text(size=15,color="black"),axis.title=ggplot2::element_text(size=15,face="bold",color="black"))+ ggplot2::theme_light(base_size = 17)+ ggpubr::grids(linetype = "dashed", color = "white")
+    #graficar efectos paqueteria ggeffects
+    dat2<- ggeffects::ggeffect(modelo)
 
-  } else if( lineas == F & puntos == T) {
+    # graficar los efectos y agruparlos en una sola grafica ggpubr
+
+    graphs2 <- lapply(names(dat2),function(x){
+      graphics::plot(dat2[[x]],connect.lines = TRUE, add.data=FALSE, colors = "bw", alpha = 0.15, dot.alpha = 0.15,jitter = 0.2,line.size = 1, show.title = FALSE) + th
+
+
+    })
+    do.call(ggpubr::ggarrange,graphs2)
+    #grafico sin lineas con puntos y con grids
+  } else if( lineas == F && puntos == T && is.null(grids)) {
     th<- ggplot2::theme(axis.text=ggplot2::element_text(size=15,color="black"),axis.title=ggplot2::element_text(size=15,face="bold",color="black"))+ ggplot2::theme_light(base_size = 17)+ ggpubr::grids(linetype = "dashed", color = "grey85")
+    #graficar efectos paqueteria ggeffects
+    dat2<- ggeffects::ggeffect(modelo)
+
+    # graficar los efectos y agruparlos en una sola grafica ggpubr
+
+    graphs2 <- lapply(names(dat2),function(x){
+      graphics::plot(dat2[[x]],connect.lines = FALSE, add.data=TRUE, colors = "bw", alpha = 0.15, dot.alpha = 0.15,jitter = 0.2,line.size = 1, show.title = FALSE) + th
+
+
+    })
+    do.call(ggpubr::ggarrange,graphs2)
+    #grafico sin lineas con puntos y sin grids
+  } else if( lineas == F && puntos == T && grids==F) {
+    th<- ggplot2::theme(axis.text=ggplot2::element_text(size=15,color="black"),axis.title=ggplot2::element_text(size=15,face="bold",color="black"))+ ggplot2::theme_light(base_size = 17)+ ggpubr::grids(linetype = "dashed", color = "white")
     #graficar efectos paqueteria ggeffects
     dat2<- ggeffects::ggeffect(modelo)
 
