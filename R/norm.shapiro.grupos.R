f119c65bc0c7a03946b1baa938a3afe7ec7a1523
mariosandoval-mx <sandoval.m@hotmail.com>
2022-03-11 00:58:31 -0600

    update

--- R/norm.shapiro.grupos.R
+++ R/norm.shapiro.grupos.R
@@ -37,51 +37,9 @@ norm.shapiro.grupos <- function(formula, datos){
   results<- merge(x = res, y = resb, all.x = TRUE)
   message(c("Prueba de normalidad de Shapiro-Wilk por grupos"))
   return(results)
-  # funcion necesaria para correr auto.mfrow
-  plot_blank <- function(main=NULL, sub=NULL, message=NULL, box=FALSE) {
-    plot(0, type="n", ann=FALSE, xaxt="n", yaxt="n", bty="n", xlim=c(0,1), ylim=c(0,1))
 
-    title(main=main, sub=sub)
 
-    if( !is.null(message) ) {
-      text(0.5,0.5, message, adj=0.5)
-    }
-
-    if( box ) {
-      box()
-    }
-  }
-  #AUTO numero de filas y columnas en graficas. Tomada de la paquetería mjcgraphics
-  auto.mfrow <- function(nplots, setup=TRUE) {
-
-    if(setup) {
-      if(nplots <= 3) par(mfrow=c(1, nplots))
-      else if(nplots <= 4)  par(mfrow=c(2,2))
-      else if(nplots <= 6)  par(mfrow=c(2,3))
-      else if(nplots <= 9)  par(mfrow=c(3,3))
-      else if(nplots <= 12) par(mfrow=c(3,4))
-      else if(nplots <= 16) par(mfrow=c(4,4))
-      else if(nplots <= 20) par(mfrow=c(4,5))
-      else if(nplots <= 25) par(mfrow=c(5,5))
-      else if(nplots <= 30) par(mfrow=c(5,6))
-      else if(nplots <= 36) par(mfrow=c(6,6))
-      else if(nplots <= 42) par(mfrow=c(6,7))
-      else if(nplots <= 49) par(mfrow=c(7,7))
-      else if(nplots <= 56) par(mfrow=c(7,8))
-      else if(nplots <= 64) par(mfrow=c(8,8))
-      else {
-        stop("Demasiadas graficas")
-      }
-    }
-    else {
-      nblankplots <- par("mfrow")[1] * par("mfrow")[2] - nplots
-      if(nblankplots > 0)
-        for(i in 1:nblankplots)
-          plot_blank()
-    }
-  }
-
-  auto.mfrow(nrow(res), setup = TRUE)
+  tlamatini:::auto_mfrow(nrow(res), setup = TRUE)
   # graficar qqplot
   plots<- aggregate(form, data = datos, FUN =
                       function(x) car::qqPlot(x,glab=deparse(substitute(groups)), ylab=deparse(substitute(x))))
